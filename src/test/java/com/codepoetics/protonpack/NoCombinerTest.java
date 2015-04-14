33da170c08ad03d55baf2d9c58abfec8460530e1
Dominic Fox <dominic.fox@opencredo.com>
2015-04-14 12:27:02 +0100

    NoCombiner, set the world afire

--- src/test/java/com/codepoetics/protonpack/NoCombinerTest.java
+++ src/test/java/com/codepoetics/protonpack/NoCombinerTest.java
@@ -2,9 +2,14 @@ package com.codepoetics.protonpack;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
 
 import static com.codepoetics.protonpack.collectors.CollectorUtils.noCombiner;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class NoCombinerTest {
 
@@ -18,4 +23,10 @@ public class NoCombinerTest {
         Long sum = IntStream.range(0, 10000).mapToObj(Long::valueOf).reduce(0L, (l1, l2) -> l1 + l2, noCombiner());
     }
 
+    @Test public void
+    can_be_used_in_a_collector() {
+        assertThat(
+                IntStream.range(0, 10000).mapToObj(Long::valueOf).collect(Collector.of(HashSet<Long>::new, Set::add, noCombiner())).size(),
+                equalTo(10000));
+    }
 }
