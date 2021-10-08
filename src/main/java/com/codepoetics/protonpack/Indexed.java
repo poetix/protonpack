67ad6bda0d3180997d93613bfec6953bf7eda7e8
Jefferson Quesado <jeff.quesado@gmail.com>
2021-10-08 14:42:23 -0300

    Added  `Indexed.mapping` and `Indexed.map`
    It would be more idiomatic to map in a stream a indexed value this way.
    
    Closes #47

--- src/main/java/com/codepoetics/protonpack/Indexed.java
+++ src/main/java/com/codepoetics/protonpack/Indexed.java
@@ -1,6 +1,7 @@
 package com.codepoetics.protonpack;
 
 import java.util.Objects;
+import java.util.function.Function;
 
 /**
  * A value combined with an index, indicating its position in an ordered sequence.
@@ -18,6 +19,28 @@ public class Indexed<T> {
      */
     public static <T> Indexed<T> index(long index, T value) {
         return new Indexed<>(index, value);
+    }
+
+    /**
+     * A factory that produces a function mapping values from indexed objects ready to use
+     * in <code>Stream.map</code>. The index is kept.
+     * @param mapper Value transformation function.
+     * @param <T> Original indexed value type.
+     * @param <U> New indexed value type.
+     * @return A function that transforms one indexed value to another, keeping the same index.
+     */
+    public static <T, U> Function<Indexed<T>, Indexed<U>> mapping(Function<T, U> mapper) {
+        return i -> i.map(mapper);
+    }
+
+    /**
+     * Map this indexed value to another value keeping the same index.
+     * @param mapper A non-interfering, stateless function to apply to the value.
+     * @param <U> The element type of the new indexed value.
+     * @return A new indexed element of the transformed value and the same index.
+     */
+    public <U> Indexed<U> map(Function<T, U> mapper) {
+        return index(index, mapper.apply(value));
     }
 
     private final long index;
